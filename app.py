ac5cac77d65d7e30a93398930f8330004ea78cb8
Maria Gorelik <marya.gorelik@gmail.com>
2023-12-21 22:24:12 +0300

    part2

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    posts = [ # список выдуманных постов
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
 
 
 if __name__ == '__main__':
