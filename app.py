de30bf17f8f79c3528d56577edadd2d76420e8ff
Maria Gorelik <marya.gorelik@gmail.com>
2023-12-24 12:05:26 +0300

    Add create and delete options

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+notes = []
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = { 'nickname': 'Miguel' } # выдуманный пользователь
-    posts = [ # список выдуманных постов
-        {
-            'author': { 'nickname': 'John' },
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': { 'nickname': 'Susan' },
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-        title = 'Home',
-        user = user,
-        posts = posts)
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if note:
+            notes.append(note)
+            return redirect(url_for('index'))
+    return render_template('index.html', notes=notes, enumerate=enumerate)
+
+
+@app.route('/delete-note/<int:note_index>/')
+def delete_note(note_index):
+    if note_index < len(notes):
+        del notes[note_index]
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+# from flask import Flask, render_template, request, redirect, url_for
+#
+# app = Flask(__name__)
+#
+# notes = []
+#
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     global notes
+#     if request.method == 'POST':
+#         note = request.form.get('note')
+#         if note:
+#             notes.append(note)
+#             return redirect(url_for('index'))
+#     #notes = ['Заметка 1', 'Заметка 2', 'Заметка 3']
+#     return render_template('index.html', notes=notes)
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
